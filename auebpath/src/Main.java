17f0cd0f1d2fa645692eb644f1e259863d8ecbe7
Karras Labros <t8230053@aueb.gr>
2024-11-24 19:33:57 +0000

    oasa info

---
+++ auebpath/src/Main.java
@@ -0,0 +1,16 @@
+package com.karkinos;
+
+public class Main {
+    public static void main(String[] args) {
+        var x = new CodeToName();
+        var y = new JSONtoString();
+        System.out.println(x.getStopName("400075"));
+        System.out.println(x.getStopXY("400075")[0]);
+        System.out.println(x.getStopXY("400075")[1]);
+        System.out.println(x.getLineName("871"));
+        System.out.println(x.getRouteName("1881"));
+        for (String z : y.getStopInfo("240071")) {
+            System.err.println(z);
+        }
+    }
+}
