310f60feef3de83de40ebee11871131966b39e5b
Karras Labros <t8230053@aueb.gr>
2024-11-24 19:42:28 +0000

    main

--- auebpath/src/main/java/Main.java
+++ auebpath/src/main/java/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+            var x = new CodeToName();
+            var y = new JSONtoString();
+            System.out.println(x.getStopName("400075"));
+            System.out.println(x.getStopXY("400075")[0]);
+            System.out.println(x.getStopXY("400075")[1]);
+            System.out.println(x.getLineName("871"));
+            System.out.println(x.getRouteName("1881"));
+            for (String z : y.getStopInfo("240071")) {
+                System.err.println(z);
+            }
     }
 }
