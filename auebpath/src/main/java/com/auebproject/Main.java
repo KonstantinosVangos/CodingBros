9b00069d5750bf2222d0c1302c8e6f9ac4bcdc55
Karras Labros <t8230053@aueb.gr>
2024-12-12 15:07:07 +0000

    diorthoseis stis Url, CodeToName

--- auebpath/src/main/java/com/auebproject/Main.java
+++ auebpath/src/main/java/com/auebproject/Main.java
@@ -2,18 +2,18 @@ package com.auebproject;
 
 public class Main {
     public static void main(String[] args) {
-            var x = new CodeToName();
-            var y = new JSONtoString();
-            var j = new Location();
-            System.out.println(x.getStopName("400075"));
-            System.out.println(x.getStopXY("400075")[0]);
-            System.out.println(x.getStopXY("400075")[1]);
-            System.out.println(x.getLineName("871"));
-            System.out.println(x.getRouteName("1881"));
-            for (String z : y.getStopInfo("240075")) {
-                System.err.println(z);
-            }
-            //overpassAPI.fetchOverpassAPIData();
-            j.location();
+        var x = new CodeToName();
+        var y = new JSONtoString();
+        var j = new Location();
+        System.out.println(x.getStopName("450028"));
+        System.out.println(x.getStopXY("110071")[0]);
+        System.out.println(x.getStopXY("110071")[1]);
+        System.out.println(x.getLineName("871"));
+        System.out.println(x.getRouteName("1881"));
+        for (String z : y.getStopInfo("240033")) {
+            System.out.println(z);
+        }
+        System.out.println("37.951596, 23.695745");
+        j.location();
     }
 }
