79167f5d7f4c9c8cc7f2eaea274c4ff176f8fcc3
LabrosKarras <t8230053@aueb.gr>
2025-01-10 15:54:53 +0000

    test

--- auebpath/src/main/java/com/auebproject/Main.java
+++ auebpath/src/main/java/com/auebproject/Main.java
@@ -10,6 +10,8 @@ public class Main {
         var asoee = new Node("asoee", 37.994124, 23.731996);
         var t1 = new Node("t1", 37.877708, 23.761687);
         System.out.println(ShortestPath.findShortestPath(asoee, t1, edges));
+        String outputFilePath = "auebpath\\src\\main\\java\\com\\auebproject\\exported_data.geojson";
+        GeoJsonExporter.exportShortestPathToGeoJson(ShortestPath.findShortestPath(asoee, t1, edges), outputFilePath);
         //j.location();
     }
 }
